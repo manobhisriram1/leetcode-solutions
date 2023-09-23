cdf391619d7209e9fb74f2a9f9231c8bf4b4aa1f
Manobhi Sriram <120110732+manobhisriram1@users.noreply.github.com>
2023-09-23 17:22:03 +0530

    Time: 2 ms (99.47%), Space: 41.9 MB (80.67%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,16 @@
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        int low = 0 , high = s.length()-1;
+        while(low<high){
+           char l = s.charAt(low) , h = s.charAt(high);
+           if(!Character.isLetterOrDigit(l)) low++;
+           else if(!Character.isLetterOrDigit(h)) high--;
+           else {
+               if(Character.toLowerCase(l)!=Character.toLowerCase(h)) return false;
+               low++; high--;
+           }  
+        }
+        return true;
+    }
+}
