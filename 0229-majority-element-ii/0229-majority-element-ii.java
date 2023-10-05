0a0e6d73013bb9cd8227ffcafd812445309e4ad4
Manobhi Sriram <120110732+manobhisriram1@users.noreply.github.com>
2023-10-05 09:36:20 +0530

    Time: 2 ms (98.52%), Space: 47.1 MB (16.25%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,7 +1,7 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-         int num1 = 0, num2 = 0, count1 = 0, count2 = 0;
-        
+        int num1 = 0, num2 = 0, count1 = 0, count2 = 0;
+
         // Step 1: Find potential majority elements
         for (int num : nums) {
             if (num == num1) {
@@ -19,7 +19,7 @@ class Solution {
                 count2--;
             }
         }
-        
+
         // Step 2: Count actual occurrences of potential majority elements
         count1 = 0;
         count2 = 0;
@@ -30,7 +30,7 @@ class Solution {
                 count2++;
             }
         }
-        
+
         // Step 3: Check if occurrences are greater than n/3
         List<Integer> result = new ArrayList<>();
         if (count1 > nums.length / 3) {
@@ -39,8 +39,8 @@ class Solution {
         if (count2 > nums.length / 3) {
             result.add(num2);
         }
-        
+
         return result;
     }
-
-    }
+}
+   
